cfcc4d8855ee7a83497ac07b08cd0ebad48d960e
Vladislav Andreev <sqandreev@yandex.ru>
2019-08-15 10:16:39 +0300

    Update useful-language

--- portfolio/useful-language/assets/scripts/index.js
+++ portfolio/useful-language/assets/scripts/index.js
@@ -10,4 +10,29 @@
   };
 
   headerMenu.addEventListener("click", showNavigation);
+
+  /**
+   * tabs
+   */
+
+  const tabsNavigationButtons = document.querySelectorAll(".courses__tabs-button");
+  const tabsContentItems = document.querySelectorAll(".courses__content-item");
+  const VISUALLY_HIDDEN = "visually-hidden";
+
+  const showCourse = (event) => {
+    const button = event.currentTarget;
+    const course = event.currentTarget.dataset.course;
+
+    tabsNavigationButtons.forEach(button => button.classList.remove("courses__tabs-button--active"));
+    button.classList.add("courses__tabs-button--active");
+
+    tabsContentItems.forEach(item => {
+      if (item.dataset.course === course) {
+        tabsContentItems.forEach(tab => tab.classList.add("visually-hidden"));
+        item.classList.remove(VISUALLY_HIDDEN);
+      }
+    });
+  };
+
+  tabsNavigationButtons.forEach(button => button.addEventListener("click", showCourse));
 })();
